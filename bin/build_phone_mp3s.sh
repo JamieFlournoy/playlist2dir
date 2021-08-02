479017f06b2fe4aad68a5970552530c0ccdebf91
Jamie Flournoy <jamie@pervasivecode.com>
2021-08-02 18:57:25 -0400

    Add a script that works on Merlin.

---
+++ bin/build_phone_mp3s.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $(uname) != "Linux" ]; then
+    echo "Run this on Linux, dummy."
+    exit 1
+fi
+
+SDCARD_VOLUME_SERIAL_NUMBER=2667-B8DA
+
+sudo /usr/local/mp3s/_scripts/playlist2dir/bin/playlist2dir \
+     /usr/local/mp3s/ \
+     /usr/local/mp3s/_playlists/_Sync\ to\ iPod.m3u8 \
+     /storage/$SDCARD_VOLUME_SERIAL_NUMBER/Music/Mp3sForPhone/
+
+echo "Now you may want to run this command on the Mac if you're copying to a mounted SD card:"
+echo "rsync -rtxi --delete /Volumes/mp3/_generated/ /Volumes/android/Music/Mp3sForPhone/"
+
+
